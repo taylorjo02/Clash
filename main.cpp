35a52806cfcd969eeed4de19a6224d564636752b
taylorjo02 <joshua.d.taylor122@gmail.com>
2025-03-31 21:57:25 -0400

    feat: draw character

--- main.cpp
+++ main.cpp
@@ -1,62 +1,49 @@
 #include "raylib.h"
-#include <raymath.h>
+#include "raymath.h"
 
 int main()
 {
-  // window dimensions
-  int windowDimensions[2];
-  windowDimensions[0] = 384;
-  windowDimensions[1] = 384;
+    const int windowWidth{384};
+    const int windowHeight{384};
+    InitWindow(windowWidth, windowHeight, "Clash");
 
-  InitWindow(windowDimensions[0], windowDimensions[1], "Clash");
+    Texture2D map = LoadTexture("nature_tileset/OpenWorldMap.png");
+    Vector2 mapPos{0.0, 0.0};
+    float speed{4.0};
 
-  // load map texture
-  Texture2D gameMap = LoadTexture("nature_tileset/OpenWorldMap.png");
-  Vector2 gmPos{0.0, 0.0};
-  float speed{4.0};
+    Texture2D knight = LoadTexture("characters/knight_idle_spritesheet.png");
+    Vector2 knightPos{
+        (float)windowWidth/2.0f - 4.0f * (0.5f * (float)knight.width/6.0f),
+        (float)windowHeight/2.0f - 4.0f * (0.5f * (float)knight.height)
+    };
 
-  Texture2D knight = LoadTexture("characters/knight_idle_spritesheet.png");
-  Vector2 knightPos{
-    (float)windowDimensions[0] / 2.0f - 4.0f * (0.5f * (float)knight.width / 6.0f),
-    (float)windowDimensions[1] / 2.0f - 4.0f * (0.5f * (float)knight.height)
-  };
-
-  SetTargetFPS(60);
-
-  while (!WindowShouldClose())
-  {
-    BeginDrawing();
-
-    ClearBackground(WHITE);
-    
-    Vector2 direction{};
-    if (IsKeyDown(KEY_A)) direction.x -= 1.0;
-    if (IsKeyDown(KEY_D)) direction.x += 1.0;
-    if (IsKeyDown(KEY_W)) direction.y -= 1.0;
-    if (IsKeyDown(KEY_S)) direction.y += 1.0;
-    
-    
-    if (Vector2Length(direction) != 0.0)
+    SetTargetFPS(60);
+    while (!WindowShouldClose())
     {
-      // set map pos = gmPos - direction
-      gmPos = Vector2Subtract(gmPos, Vector2Scale(Vector2Normalize(direction), speed));
+        BeginDrawing();
+        ClearBackground(WHITE);
+
+        Vector2 direction{};
+        if (IsKeyDown(KEY_A)) direction.x -= 1.0;
+        if (IsKeyDown(KEY_D)) direction.x += 1.0;
+        if (IsKeyDown(KEY_W)) direction.y -= 1.0;
+        if (IsKeyDown(KEY_S)) direction.y += 1.0;
+        if (Vector2Length(direction) != 0.0)
+        {
+            // set mapPos = mapPos - direction
+            
+            mapPos = Vector2Subtract(mapPos, Vector2Scale(Vector2Normalize(direction), speed));
+        }
+        
+        // draw the map
+        DrawTextureEx(map, mapPos, 0.0, 4.0, WHITE);
+
+        // draw the character
+        Rectangle source{0.f, 0.f, (float)knight.width/6.f, (float)knight.height};
+        Rectangle dest{knightPos.x, knightPos.y, 4.0f * (float)knight.width/6.0f, 4.0f * (float)knight.height};
+        DrawTexturePro(knight, source, dest, Vector2{}, 0.f, WHITE);
+
+        EndDrawing();
     }
-    
-    // draw the knight
-    Rectangle source = {
-      0.f, 0.f, (float)knight.width / 6.f, (float)knight.height
-    };
-  
-    Rectangle dest = {
-      knightPos.x, knightPos.y, 4.f * (float)knight.width/6.f , 4.f * (float)knight.height
-    };
-
-    DrawTexturePro(knight, source, dest, Vector2{}, 0.f, WHITE);
-    // draw the map
-    DrawTextureEx(gameMap, gmPos, 0.0, 4.0, WHITE);
-
-    EndDrawing();
-  }
-  UnloadTexture(gameMap);
-  CloseWindow();
+    CloseWindow();
 }
